3b497d7cd5e2425178151f9d6c0e689bd82f4e80
ritvik sahajpal <ritvik@umd.edu>
2016-07-27 14:52:59 -0400

    new functiuon to combine multiple images into ne big one

---
+++ pygeoutil/util_plot.py
@@ -0,0 +1,31 @@
+import math
+import os
+from PIL import Image
+
+
+def combine_plots_into_one(all_val_imgs, path_out, out_fname, imgs_in_row=10.):
+    """
+    Concatenate all images to one giant image
+    Args:
+        all_val_imgs:
+        path_out:
+        out_fname:
+        imgs_in_row:
+
+    Returns:
+
+    """
+    width, height = Image.open(all_val_imgs[0]).size
+    max_size = max(width, height)
+
+    # assuming that all images have same size
+    new_im = Image.new('RGB', (int(max_size * imgs_in_row), int(max_size * math.ceil(len(all_val_imgs)/imgs_in_row))))
+
+    for idx, img in enumerate(all_val_imgs):
+        x_pos = int(width * (idx % imgs_in_row))  # upper left of image
+        y_pos = int(height * (idx / imgs_in_row))  # upper top of image
+
+        im = Image.open(img)
+        new_im.paste(im, (x_pos, y_pos))
+
+    new_im.save(path_out + os.sep + out_fname + '.png')
