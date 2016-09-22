329b8af6e6d515238e9c7005400c59cb7faf0ee6
ritviksahajpal <ritvik@umd.edu>
2016-09-22 12:38:48 -0400

    make_dir_if_missing now returns directory path

--- pygeoutil/util.py
+++ pygeoutil/util.py
@@ -459,6 +459,7 @@ def make_dir_if_missing(d):
     """
     try:
         os.makedirs(d)
+        return d
     except OSError as exception:
         if exception.errno != errno.EEXIST:
             raise
