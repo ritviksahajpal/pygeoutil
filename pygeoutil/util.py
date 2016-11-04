5094ca5e7b5c632bf861c9581cf54bf4523e6dc3
ritvik sahajpal <ritvik@umd.edu>
2016-11-04 11:49:29 -0400

    minor tweaks to IAM pre-preocessing

--- pygeoutil/util.py
+++ pygeoutil/util.py
@@ -1514,7 +1514,7 @@ def modify_nc_val(path_inp, var, new_val):
         hndl_inp[var][:] = new_val
 
 
-def merge_nc_files(list_nc_files, path_out_nc, common_var_name='', replace_var_by_file_name=False, fill_val=np.NaN):
+def merge_nc_files(list_nc_files, path_out_nc, common_var_name='', replace_var_by_file_name=False):
     """
 
     Args:
@@ -1528,9 +1528,6 @@ def merge_nc_files(list_nc_files, path_out_nc, common_var_name='', replace_var_b
     """
     list_dims = []  # List of dimensions in input netCDF file
     list_vars = []
-
-    if os.path.exists(path_out_nc):
-        return
 
     with open_or_die(path_out_nc, perm='w') as hndl_out_nc:
         for fl in list_nc_files:
@@ -1549,12 +1546,10 @@ def merge_nc_files(list_nc_files, path_out_nc, common_var_name='', replace_var_b
                             new_name_var = os.path.splitext(os.path.basename(fl))[0]
                             list_vars.append(new_name_var)
 
-                            out_var = hndl_out_nc.createVariable(new_name_var, var.datatype, var.dimensions, zlib=True,
-                                                                 fill_value=fill_val)
+                            out_var = hndl_out_nc.createVariable(new_name_var, var.datatype, var.dimensions, zlib=True)
                         else:
                             list_vars.append(name_var)
-                            out_var = hndl_out_nc.createVariable(name_var, var.datatype, var.dimensions, zlib=True,
-                                                                 fill_value=fill_val)
+                            out_var = hndl_out_nc.createVariable(name_var, var.datatype, var.dimensions, zlib=True)
 
                         # Copy variable attributes
                         out_var.setncatts({k: var.getncattr(k) for k in var.ncattrs()})
